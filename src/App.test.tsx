224100dd4639e3c6936b8d172d8cf5d5c21b8f98
itsmephani <vs3.phani@gmail.com>
2020-08-29 09:37:15 +0530

    Initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app with header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = getByText(/Rally Health India/i);
+
+  expect(headerElement).toBeInTheDocument();
 });
